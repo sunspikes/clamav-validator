be74fba4c28c2ef578b45dced254a8322b4fe437
Ricardo Čerljenko <ricardo@lloyds-digital.com>
2022-08-30 10:36:41 +0200

    added null protection

--- src/ClamavValidator/ClamavValidator.php
+++ src/ClamavValidator/ClamavValidator.php
@@ -45,7 +45,7 @@ class ClamavValidator extends Validator
      */
     public function validateClamav(string $attribute, $value, array $parameters): bool
     {
-        if (true === Config::get('clamav.skip_validation')) {
+        if (! $value || true === Config::get('clamav.skip_validation')) {
             return true;
         }
 
